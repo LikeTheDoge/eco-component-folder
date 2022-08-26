3abd9c3b59fd079c0193c53f9ee0332abf960b8f
like_the_doge <like_the_doge@outlook.com>
2022-08-26 10:00:39 +0800

    修改 gitignore

--- packages/eco-components-file/packages/EcoIcon/src/Index.vue
+++ packages/eco-components-file/packages/EcoIcon/src/Index.vue
@@ -1,38 +1,43 @@
 <template>
-  <svg
-  class="eco-icon"
-  :style="{fontSize:`${typeof size === 'number'?size+'px':size }`,color:color}"
-  aria-hidden="true">
-      <use :xlink:href="`#icon-${name}`"></use>
-  </svg>
+    <svg
+        class="eco-icon"
+        :style="{fontSize:`${typeof size === 'number'?size+'px':size }`,color:color}"
+        aria-hidden="true"
+    >
+        <use :xlink:href="`#${family}-${name}`"></use>
+    </svg>
 </template>
 
 <script>
-import * as icon from './icon';
+import * as icon from "./icon";
 
 console.log(icon);
 
 export default {
-  name: 'EcoIcon',
-  props: {
-    size: {
-      default: 'inherit',
+    name: "EcoIcon",
+    props: {
+        family: {
+            default: "icon",
+        },
+        size: {
+            default: "inherit",
+        },
+        name: {
+            default: "search",
+        },
+        color: {
+            default: "inherit",
+        },
     },
-    name: {
-      default: 'search',
-    },
-    color: {
-      default: 'inherit',
-    },
-  },
 };
 </script>
 
 <style>
-    .eco-icon {
-       width: 1em; height: 1em;
-       vertical-align: -0.15em;
-       fill: currentColor;
-       overflow: hidden;
-    }
+.eco-icon {
+    width: 1em;
+    height: 1em;
+    vertical-align: -0.15em;
+    fill: currentColor;
+    overflow: hidden;
+}
 </style>
