8b3b8c64eda00c3787b2264886e322efb048dac2
like_the_doge <like_the_doge@outlook.com>
2022-09-01 16:37:02 +0800

    Update models.js

--- packages/eco-components-file/packages/models.js
+++ packages/eco-components-file/packages/models.js
@@ -1,26 +1,26 @@
 export class EFile {
 
-    static extensionType = (ext) => {
-        const exts = {
-            'word': ['doc'],
-            'excel': ['excel'],
-            'ppt': ['ppt'],
-            'music': ['mp3'],
-            'video': ['mp4'],
-            'html': ['html'],
-            'gif': ['gif'],
-            'exe': ['exe'],
-            'pdf': ['pdf'],
-            'txt': ['txt'],
-        }
-        const list = Object.entries(exts).flatMap(([type, exts]) => {
-            return exts.map(ext => { ext, type })
-        })
+    // static extensionType = (ext) => {
+    //     const exts = {
+    //         'word': ['doc'],
+    //         'excel': ['excel'],
+    //         'ppt': ['ppt'],
+    //         'music': ['mp3'],
+    //         'video': ['mp4'],
+    //         'html': ['html'],
+    //         'gif': ['gif'],
+    //         'exe': ['exe'],
+    //         'pdf': ['pdf'],
+    //         'txt': ['txt'],
+    //     }
+    //     const list = Object.entries(exts).flatMap(([type, exts]) => {
+    //         return exts.map(ext => { ext, type })
+    //     })
 
-        const extension = (ext || '').trim()
-        const type = list.find(v => extension.indexOf(v.ext) === 0)
+    //     const extension = (ext || '').trim()
+    //     const type = list.find(v => extension.indexOf(v.ext) === 0)
 
-        return type || 'unknown'
-    }
+    //     return type || 'unknown'
+    // }
     type = 'unknown'
 }
